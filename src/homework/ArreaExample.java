0834f60be510cfd88eba68c2f25287c6181da226
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-07 13:52:14 +0300

    123

--- src/homework/ArreaExample.java
+++ src/homework/ArreaExample.java
@@ -13,16 +13,16 @@ public class ArreaExample {
     public static void main(String[] args) {
 
         char a = 42;
-
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
+
                 System.out.print(a + " ");
             }
             System.out.println(" ");
         }
     }
 
-    }
+}
 
 
 

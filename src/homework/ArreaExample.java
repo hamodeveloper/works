f0776fbf9217375e519e15051638786bee70f1c0
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-07 10:41:06 +0300

    123

---
+++ src/homework/ArreaExample.java
@@ -0,0 +1,45 @@
+package homework;
+
+import com.sun.org.apache.xpath.internal.objects.XBoolean;
+
+public class ArreaExample {
+
+    /*
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+     */
+
+    public static void main(String[] args) {
+
+char a = 42;
+        System.out.println("*");
+for (char i = 42; i < 43; i++) {
+
+    System.out.println(a + " *");
+
+}
+        for (char j = 42; j < 43; j++) {
+            System.out.println(a + " * *");
+        }
+        for (char j = 42; j < 43; j++) {
+            System.out.println(a + " * * *");
+        }
+        for (char j = 42; j < 43; j++) {
+            System.out.println(a + " * * * *");
+        }
+
+
+        System.out.println();
+                    }
+
+            }
+
+
+
+
+
+
+

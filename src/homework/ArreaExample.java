df045f2552327d820d8a783f5a37a522663294c6
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-08 11:43:06 +0300

    1234

--- src/homework/ArreaExample.java
+++ src/homework/ArreaExample.java
@@ -12,15 +12,12 @@ public class ArreaExample {
 
     public static void main(String[] args) {
 
-        for (int i = 4; i >= 0; i--) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 4; i >=0 ; i--) {
+            for (int j = i; j <=4 ; j++) {
                 System.out.print(" *");
 
             }
-
             System.out.println(" ");
-
-
         }
 
 

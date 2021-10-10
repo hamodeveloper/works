416880f12807e7f2e6632d9b592a6955519ca0f4
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-10 22:11:34 +0300

    Figures

--- src/homework/ArreaExample.java
+++ src/homework/ArreaExample.java
@@ -11,7 +11,7 @@ public class ArreaExample {
      */
 
     public static void main(String[] args) {
-
+/*
         for (int i = 4; i >=0 ; i--) {
             for (int j = i; j <=4 ; j++) {
                 System.out.print(" *");
@@ -22,13 +22,15 @@ public class ArreaExample {
 
         }
 
-        System.out.println(" ");
+       System.out.println(" ");
         /*
         * * * *
         * * *
         * *
         *
          */
+
+/*
         for (int i = 5; i >= 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" *");
@@ -40,15 +42,81 @@ public class ArreaExample {
 
         }
 
+*/
+        /*
+         *
+         * *
+         * * *
+         * * * *
+         */
+/*
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+*/
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print(" ");
+            }
+
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j >=i ; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
 
 
 
 
 
 
+        /*
+          *
+         * *
+       * * * *
+      * * * * *
+       * * * *
+        * * *
+         * *
+          *
+         */
+
+/*
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *  ");
+            }
 
 
+            System.out.println();
+        }
 
+        for (int j = 3; j >= 0; j--) {
+            for (int k = 4; k > j; k--) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= j; k++) {
+                System.out.print(" *  ");
+            }
+
+            System.out.println();
+        }
+
+*/
     }
 
 }

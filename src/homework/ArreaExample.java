c6a280bc46e4fd2aa9697d2de4e88a2b4b09268f
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-10 22:20:45 +0300

    Figures

--- src/homework/ArreaExample.java
+++ src/homework/ArreaExample.java
@@ -62,18 +62,10 @@ public class ArreaExample {
 */
 
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <=i; j++) {
-                System.out.print(" ");
-            }
 
-        }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 3; j >=i ; j--) {
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
+
+
+
 
 
 

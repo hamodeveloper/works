27681535f7189a59520cfd46b720d1af0bb1799b
hamodeveloper <lemuchuyamuk@gmail.com>
2021-10-16 19:42:43 +0300

    Figures

---
+++ src/homework/ArrayUtil.java
@@ -0,0 +1,52 @@
+package homework;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+
+
+
+
+/* bolor kent tvery
+        System.out.print("մասիվի բոլոր կենտ էլեմենտները` ");
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] % 2 != 0) {
+
+                System.out.print(numbers[i] + " ");
+
+            }
+*/
+        /* bolor zuyg tvery
+        System.out.print("մասիվի բոլոր զույգ էլեմենտները` ");
+        for (int i = 0; i < numbers.length; i++) {
+
+ if (numbers[i] % 2 == 0) {
+
+     System.out.print(numbers[i] + " ");
+
+ }
+           */
+            /* bolor tvery
+            int k = numbers[i];
+            System.out.println(k);
+*/
+
+            /* amenamec tivy
+             if(k >= 66) {
+                System.out.println("մասիվի ամենամեծ թիվը " + k);
+            }
+*/
+
+/* gumary
+            System.out.println(numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5]+numbers[6]+numbers[7]+numbers[8]+numbers[9]);
+*/
+
+
+
+    }
+
+
+}
